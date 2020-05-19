9899ac87529adaf0da1713052b25fbff6ca8314d
August <mingxin.android@gmail.com>
2020-05-19 16:21:55 +0800

    openwrt: change ipaddr from 192.168.x.x to 172.16.x.x

--- Openwrt/target/linux/rtd1295/base-files.mk
+++ Openwrt/target/linux/rtd1295/base-files.mk
@@ -1,7 +1,7 @@
 define Package/base-files/install-target
 
  ifeq ($(CONFIG_RTD_1295_HWNAT),y)
-	sed -i 's|192.168.0.9|192.168.1.9|g' $(1)/etc/config/network
+	sed -i 's|172.16.0.9|172.16.1.9|g' $(1)/etc/config/network
 	cat $(PLATFORM_DIR)/conf/network.nat >> $(1)/etc/config/network
  else
 	sed -i 's|wan|lan|g' $(1)/etc/config/network
