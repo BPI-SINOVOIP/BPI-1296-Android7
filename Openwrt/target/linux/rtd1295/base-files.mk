a5c519375a41002c6960bda2ec7159dc97e934e7
August <mingxin.android@gmail.com>
2020-06-02 09:28:01 +0800

    openwrt: set lan gateway ip to 172.16.1.1

--- Openwrt/target/linux/rtd1295/base-files.mk
+++ Openwrt/target/linux/rtd1295/base-files.mk
@@ -1,7 +1,7 @@
 define Package/base-files/install-target
 
  ifeq ($(CONFIG_RTD_1295_HWNAT),y)
-	sed -i 's|172.16.0.9|172.16.1.9|g' $(1)/etc/config/network
+	sed -i 's|172.16.0.1|172.16.1.1|g' $(1)/etc/config/network
 	cat $(PLATFORM_DIR)/conf/network.nat >> $(1)/etc/config/network
  else
 	sed -i 's|wan|lan|g' $(1)/etc/config/network
